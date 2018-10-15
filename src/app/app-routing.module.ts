b8198bda5e43d922fbab98548d2b826f78ab1762
Natanael de Sousa Leite <natanael.leite@serpro.gov.br>
2018-10-15 09:41:02 -0300

    removido o facebook por estar provocando erros na compilação de produção do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,32 +3,42 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardService } from './service/auth-guard.service';
 
 const routes: Routes = [
-  { 
-    path: '', 
+  {
+    path: '',
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  { 
-    path: 'home', 
+  {
+    path: 'home',
     canActivate: [AuthGuardService],
-    loadChildren: './tabs/tabs.module#TabsPageModule',
+    loadChildren: './tabs/tabs.module#TabsPageModule'
 
   },
-  { 
+  {
     path: 'register',
     loadChildren: './register/register.module#RegisterPageModule'
   },
-  { 
+  {
     path: 'login',
     loadChildren: './login/login.module#LoginPageModule'
   },
-  { 
-    path: '**', 
+  {
+    path: '**',
     loadChildren: './page-not-found/page-not-found.module#PageNotFoundPageModule'
   },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'Busca', loadChildren: './busca/busca.module#BuscaPageModule' }
+  {
+    path: 'profile',
+    loadChildren: './profile/profile.module#ProfilePageModule' 
+  },
+  {
+    path: 'register',
+    loadChildren: './register/register.module#RegisterPageModule' 
+  },
+  {
+    path: 'Busca',
+    loadChildren: './busca/busca.module#BuscaPageModule'
+  }
+
 ];
 
 @NgModule({
