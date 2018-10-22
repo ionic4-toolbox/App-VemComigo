695b37589a86d7c575307d2ae9545bd8185ac2db
Natanael Leite <natansl@gmail.com>
2018-10-21 21:55:26 -0300

    Correções pos reunião com as steakholders

--- src/app/busca/busca.page.ts
+++ src/app/busca/busca.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DestinoService } from '../service/destinos.service';
 import { Destino } from './../models/destino.model';
+import { NavController } from '@ionic/angular';
+import { PerfilPage } from '../perfil/perfil.page';
 
 @Component({
   selector: 'app-busca',
@@ -11,7 +13,10 @@ export class BuscaPage implements OnInit {
   btnBuscar = true;
   destinos: Destino[];
 
-  constructor(private destinoService: DestinoService) { }
+  constructor(
+    private destinoService: DestinoService,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
     this.destinoService.getDestinos().subscribe(data => {
@@ -23,4 +28,15 @@ export class BuscaPage implements OnInit {
   exibirBusca(param: boolean) {
     this.btnBuscar = false;
   }
+
+  editar() {
+    console.log('Editar');
+    this.navCtrl.navigateRoot('/home/tabs/(perfil:perfil)');
+  }
+
+  ajuda() {
+    console.log('Editar');
+    this.navCtrl.navigateRoot('/home/tabs/(ajuda:ajuda)');
+  }
+
 }
